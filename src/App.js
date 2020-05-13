eaa29cb0fd5b9e2abe5bf7de994fb3f569c6a24b
Carson Keller <carsonjkeller@gmail.com>
2020-05-13 15:52:51 -0600

    Initial commit and reorganization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 import './App.css';
 
 function App() {
@@ -21,6 +23,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
