7177342adac5cfab0b908dc5a098bd9fd0c253f8
Carson Keller <carsonjkeller@gmail.com>
2020-05-19 08:24:33 -0600

    Updated App function to include context provider and page routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-
+import { ListsDisplay } from './pages/Home';
+import { CalendarDisplay } from './pages/CalendarPage';
+import { GenerateList } from './pages/ListPage';
+import { ListsContext } from './context/ListsContext';
 import './App.css';
 
 function App() {
+  const { Provider } = ListsContext;
+  const [lists, setLists] = useState([]);
+  const [selectedList, setSelectedList] = useState({tasks: []});
+  const getLists = () => {
+    const url = process.env.REACT_APP_API_URL;
+    fetch(`${url}`)
+        .then(response => response.json())
+        .then(data => {
+          setLists(data);
+          if (selectedList._id) {
+            const listArray = data.filter((list) => selectedList._id === list._id);
+            setSelectedList(listArray[0]);
+        }})
+        .catch(err => err);
+  };
+
+  useEffect(() => {getLists()}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={{ lists, getLists, selectedList, setSelectedList }}>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={ListsDisplay} />
+          <Route exact path='/calendar' component={CalendarDisplay} />
+          <Route exact path='/list' component={GenerateList} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
